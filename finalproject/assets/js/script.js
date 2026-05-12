d2959f6bd3d978e2b9b9b92f34e10be73d4a15bb
basshitt84 <157418893+basshitt84@users.noreply.github.com>
2026-05-12 04:19:26 +0000

    gallery and footer added

--- finalproject/assets/js/script.js
+++ finalproject/assets/js/script.js
@@ -15,3 +15,40 @@ albumCards.forEach(function (card) {
     card.classList.toggle("is-flipped");
   });
 });
+const carouselImages = document.querySelectorAll(".carousel-track img");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let currentImage = 0;
+
+function showImage(index) {
+  carouselImages.forEach(function (image) {
+    image.classList.remove("active");
+  });
+
+  carouselImages[index].classList.add("active");
+}
+
+if (carouselImages.length > 0) {
+  showImage(currentImage);
+
+  nextBtn.addEventListener("click", function () {
+    currentImage = currentImage + 1;
+
+    if (currentImage >= carouselImages.length) {
+      currentImage = 0;
+    }
+
+    showImage(currentImage);
+  });
+
+  prevBtn.addEventListener("click", function () {
+    currentImage = currentImage - 1;
+
+    if (currentImage < 0) {
+      currentImage = carouselImages.length - 1;
+    }
+
+    showImage(currentImage);
+  });
+}
